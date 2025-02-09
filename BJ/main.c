8e6d76873ce16a9ec8792168800471d935383899
Andy Hong <chili.tomat0@icloud.com>
2025-02-09 18:18:51 +0900

    250209

--- BJ/main.c
+++ BJ/main.c
@@ -7,20 +7,6 @@
 
 #include <stdio.h>
 
-int dp[11] = {-1, 1, 2, 4, };
-
-int recur(int i) {
-    if (dp[i]) return dp[i];
-    return dp[i] = recur(i - 1) + recur(i - 2) + recur(i - 3);
-}
-
 int main(void) {
-    int t;
-    scanf("%d", &t);
-    while (t--) {
-        int n;
-        scanf("%d", &n);
-        printf("%d\n", recur(n));
-    }
     return 0;
 }
---
+++ BJ2444/main.py
@@ -0,0 +1,15 @@
+#
+#  별 찍기 - 7
+#  BJ
+#
+#  Created by 준우 on 2/9/25.
+#
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+for i in range(1, n + 1):
+    print(' ' * (n - i) + '*' * (2 * i - 1))
+for i in range(n - 1, 0, -1):
+    print(' ' * (n - i) + '*' * (2 * i - 1))
